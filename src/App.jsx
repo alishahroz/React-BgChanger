2876da30b9d3c7bb3f08ed619b9573f68fde93ce
alishahroz <alishahrozbhatti@gmil.com>
2024-12-18 12:41:14 +0500

    uploading

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,26 @@ function App() {
   return (
     <>
       <h1>hello</h1>
-      
+      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img class="w-24 h-24 rounded-full mx-auto" src="https://img.freepik.com/free-photo/breathtaking-view-forest-covered-with-snow-sunset-norway_181624-45356.jpg?t=st=1734357010~exp=1734360610~hmac=9b5e6e7f6801b26311cab81962306db0fd12d8a550b13899f8c86c80f089c32a&w=900" alt="" width="384" height="512" />
+  <div class="pt-6 space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption>
+      <div>
+        Sarah Dayan
+      </div>
+      <div>
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
     </>
   )
 }
